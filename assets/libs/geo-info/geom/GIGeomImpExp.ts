7239a69e30190afd35edc48538ac65a42c40ecd4
phuongtung1 <phuongtung1231992@yahoo.com>
2021-02-17 14:48:17 +0800

    Auto-generated commit

--- assets/libs/geo-info/geom/GIGeomImpExp.ts
+++ assets/libs/geo-info/geom/GIGeomImpExp.ts
@@ -16,8 +16,8 @@ export class GIGeomImpExp {
         this._geom_maps = geom_maps;
     }
     /**
-     * Import GI data into this model, and renumber teh entities in the process.
-     * @param other_geom_maps The data to import
+     * Import GI data into this model
+     * @param other_geom_maps The geom_arrays of the other model.
      */
     public importGIRenum( gi_data: IGeomJSONData ): IRenumMaps {
         // positions
--- polyfills-es2015.abd5f162b180f820c409.js
+++ polyfills-es2015.abd5f162b180f820c409.js
@@ -8124,7 +8124,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\patrick\Documents\Repos\mobius\mobius-parametric-modeller-dev-0-7\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\akibdpt\Documents\Angular\mobius-parametric-modeller-dev-0-7\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
